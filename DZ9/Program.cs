bcf5e7693b90661874588b364a5fa41824605725
Nikita <tesh-123@mail.ru>
2022-10-12 18:42:33 +0300

    Добавил домашнее задание по 9 семинару

---
+++ DZ9/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 64
+/*Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNumbersToN(number);
+
+void PrintNumbersToN(int n)
+{
+    if (n < 1) return;
+    Console.Write(n + " ");
+    PrintNumbersToN(n - 1);
+}
+*/
+//Задача 66
+/*Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM > numberN)
+{
+    Console.WriteLine("Число M не может быть больше N");
+}
+else
+{
+    int sum = SumCountFromMToN(numberN, numberM);
+    Console.WriteLine(sum);
+}
+
+int SumCountFromMToN(int n, int m)
+{
+    if (n < m) return 0;
+    int sum = SumCountFromMToN(n - 1, m);
+    return sum += n;
+}
+*/
+//Задача 68
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM >= 0 && numberN >= 0)
+{
+    int sum = SumCountFromMToN(numberN, numberM);
+    Console.WriteLine(sum);
+}
+else
+{
+    Console.WriteLine("Число M и N не могут быть отрицательными");
+}
+
+int SumCountFromMToN(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if (m > 0 && n == 0)
+    {
+        return SumCountFromMToN(m - 1, 1);
+    }
+    else
+    {
+        return SumCountFromMToN(m - 1, SumCountFromMToN(m, n - 1));
+    }
+}
